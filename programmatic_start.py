a32ae36b61bfad1131a6a91b6593ede5d2c5860a
Mike Nielsen <mmni@protonmail.com>
2023-12-12 19:56:34 +0100

    openapi description

--- programmatic_start.py
+++ programmatic_start.py
@@ -2,8 +2,11 @@ import asyncio
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+ID_GENERATOR = 10
 
 
 @app.get("/")
@@ -14,8 +17,25 @@ def read_root():
     }
 
 
+class Alien(BaseModel):
+    id: int
+    type: str
+    name: str
+    weight: float
+
+
+@app.post("/create")
+def createAlien(alien: Alien):
+    global ID_GENERATOR
+    ID_GENERATOR = ID_GENERATOR + 1
+    print(f"Create this alien ${alien}")
+    print(f"This is a string ${alien.model_dump_json()}")
+    newalien = Alien(id=ID_GENERATOR, type=alien.type, name=alien.name, weight=alien.weight)
+    return newalien
+
+
 async def main():
-    config = uvicorn.Config("programmatic_start:app", port=8000, log_level="info")
+    config = uvicorn.Config("programmatic_start:app", port=8000, log_level="info", reload=True)
     server = uvicorn.Server(config)
     await server.serve()
 
